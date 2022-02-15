b40054bb3ad6aaa75a13390ea266b978c51a7b4d
ck <hans1804@daum.net>
2022-02-15 23:56:12 +0900

    initial import

---
+++ ClientApp/src/inflearn/components/SeeSharpGuide.js
@@ -0,0 +1,17 @@
+﻿import React from 'react';
+import SeeSharpGuideTitle from "./SeeSharpGuideTitle";
+
+function SeeSharpGuide() {
+    return (
+        <div>
+            <SeeSharpGuideTitle/>
+            <ul>
+                <li>리엑트 소개</li>
+                <li>리엑트 개발환경 구축</li>
+                <li>리엑트 Hello World</li>
+            </ul>
+        </div>
+    );
+}
+
+export default SeeSharpGuide;
---
+++ ClientApp/src/inflearn/components/SeeSharpGuideTitle.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import '../css/App.css';
+
+function SeeSharpGuideTitle() {
+    const subject = 'React';
+    const title = `${subject} 학습 가이드`
+    const titleStyles = { color: '#C0FFEE', textDecoration: 'underline' };
+    return (
+        <header className="seesharp-guide-title">
+            <h2 style={titleStyles}>{ title }</h2>
+        </header>
+    );
+}
+
+export default SeeSharpGuideTitle
